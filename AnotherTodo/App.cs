ca5800e270490a7aaa294a2213654ce753e9532f
Luis Rocha <lerocha@gmail.com>
2014-11-24 22:56:39 -0800

    initial version

---
+++ AnotherTodo/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AnotherTodo
+{
+	public class App
+	{
+		public static Page GetMainPage()
+		{	
+			return new ContentPage { 
+				Content = new Label {
+					Text = "Hello, AnotherTodo!",
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+				},
+			};
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+## AnotherTodo
+
+Todo app implemented using [Xamarin][1] that consumes [Google Tasks API][2].
+
+
+```
+  [1]: http://developer.xamarin.com/
+  [2]: https://developers.google.com/google-apps/tasks/
